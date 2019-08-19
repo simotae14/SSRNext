7aa217f8b241dbc016b7db944b880961ccf61718
Simone Taeggi <startae14@gmail.com>
2019-08-19 23:02:56 +0200

    Add the reducer

--- store.js
+++ store.js
@@ -23,6 +23,24 @@ export const addItem = item => {
   };
 }
 
+// reducers
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'INITIALCARDS':
+      return {
+        cards: action.cards
+      };
+    case 'ADD':
+      return {
+        ...state,
+        // add the item to the actual state
+        cards: [...state.cards, action.item]
+      };
+    default:
+      return state;
+  }
+}
+
 // create the initial store
 export const initStore = (initialState = startState) => {
   return createStore(reducer, initialState, composeWithDevTools(applyMiddleware(thunkMiddleware)));
